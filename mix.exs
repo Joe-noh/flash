4ba0fe1be4a214ca96a9895ea5c6fda20f3c065d
Joe-noh <honzawa.j@pepabo.com>
2015-11-10 18:37:49 +0900

    exrm使わなくなったので吹き飛ばした

--- mix.exs
+++ mix.exs
@@ -29,13 +29,13 @@ defmodule Flash.Mixfile do
   #
   # Type `mix help deps` for examples and options
   defp deps do
-    [{:phoenix, "~> 1.0.2"},
-     {:phoenix_ecto, "~> 1.1"},
-     {:postgrex, ">= 0.0.0"},
-     {:phoenix_html, "~> 2.1"},
-     {:phoenix_live_reload, "~> 1.0", only: :dev},
-     {:cowboy, "~> 1.0"},
-
-     {:exrm, "~> 0.19"}]
+    [
+      {:phoenix, "~> 1.0.2"},
+      {:phoenix_ecto, "~> 1.1"},
+      {:postgrex, ">= 0.0.0"},
+      {:phoenix_html, "~> 2.1"},
+      {:phoenix_live_reload, "~> 1.0", only: :dev},
+      {:cowboy, "~> 1.0"}
+    ]
   end
 end
