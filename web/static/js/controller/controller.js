0015b8d652811061e10c24d234f1aa185a4be637
Joe-noh <honzawa.j@pepabo.com>
2015-12-10 13:36:54 +0900

    controllerがやることを整理したのであった

--- web/static/js/controller/controller.js
+++ web/static/js/controller/controller.js
@@ -2,33 +2,24 @@ import "deps/phoenix_html/web/static/js/phoenix_html";
 
 import socket from "../shared/socket";
 
-let channel = socket.channel("rooms:lobby", {});
+let channel = socket.channel("rooms:lobby", {operator: true});
 
 $(document).ready(() => {
+  channel.on('current', (params) => {
+    console.log(params);
+
+    channel.push('current:ok', {});
+  });
+
+  channel.on("recipe", (recipe) => {
+    console.log(recipe);
+  });
+
+  channel.on("start:ok", ()) => {
+    console.log("started");
+  });
+
   channel.join()
-    .receive("ok", (resp) => {
-      console.log("Joined successfully");
-    })
-    .receive("error", resp => {
-      console.log("Unable to join", resp)
-    });
-
-  $('#ping').click((e) => {
-    e.preventDefault();
-
-    channel.push("ping:all", {});
-  });
-
-  $('#color-code-button').click((e) => {
-    e.preventDefault();
-
-    let code = $('#color-code').val();
-    channel.push("color:change", {code: code});
-  });
-
-  $('#opacity-range').on('input', (e) => {
-    let opacity = e.target.value / 100;
-    console.log(opacity);
-    channel.push("opacity:change", {opacity: opacity});
-  });
+    .receive("ok",    (resp) => { console.log("Joined successfully"); })
+    .receive("error", (resp) => { console.log("Unable to join", resp) });
 });
