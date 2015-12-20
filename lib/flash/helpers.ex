386f55f9e13a4f84303a93d43f6abbc3ba2400b0
Joe-noh <goflb.jh@gmail.com>
2015-12-20 12:53:05 +0900

    こうやって光ってくれ指示を簡潔に
    ランタイムのパフォーマンスを捨てた

---
+++ lib/flash/helpers.ex
@@ -0,0 +1,39 @@
+defmodule Flash.Helpers do
+  @doc """
+  :switchアクションを繰り返し生成する
+  """
+  def switch_cycle(count, colors, period, start_at) do
+    length = length(colors)
+    Enum.map 0..count-1, fn i ->
+      color = Enum.at(colors, rem(i, length))
+      {start_at + period*i, :switch, color}
+    end
+  end
+
+  @doc """
+  :fadeアクションを繰り返し生成する
+  """
+  def fade_cycle(count, colors, period, start_at) do
+    length = length(colors)
+    Enum.map 0..count-1, fn i ->
+      color = Enum.at(colors, rem(i, length))
+      {start_at + period*i, :fade, color, period}
+    end
+  end
+
+  def expand_score({start_at, :switch, color}) do
+    %{start_at: start_at, detail: %{type: :switch, color: color}}
+  end
+
+  def expand_score({start_at, :fade, color, duration}) do
+    %{start_at: start_at, detail: %{type: :fade, color: color, duration: duration}}
+  end
+
+  def expand_score({start_at, :rainbow}) do
+    %{start_at: start_at, detail: %{type: :rainbow}}
+  end
+
+  def bpm(n), do: n * 1000 / 60
+
+  def offset(bpm, count), do: count * 60000 / bpm
+end
--- lib/flash/manager.ex
+++ lib/flash/manager.ex
@@ -1,43 +1,19 @@
 defmodule Flash.Manager do
   use GenServer
 
+  import Flash.Helpers
+
   @skyblue "#6fe3fc"
-  @blue   "#22A7F0"
-  @pink  "#fd92be"
-  @black  "#101010"
+  @blue    "#22a7f0"
+  @pink    "#fd92be"
+  @black   "#101010"
 
-  expand_score = fn
-    {start_at, :switch, color} ->
-      %{start_at: start_at, detail: %{type: :switch, color: color}}
-    {start_at, :fade, color, duration} ->
-      %{start_at: start_at, detail: %{type: :switch, color: color, duration: duration}}
-    {start_at, :rainbow} ->
-      %{start_at: start_at, detail: %{type: :rainbow}}
+  def black_out do
+    [
+      {  10, :switch, @black},
+      {1000, :switch, @black}
+    ] |> Enum.map(&expand_score/1)
   end
-
-  @scores [
-    {   10, :switch, @pink},
-    { 1000, :switch, @skyblue},
-    { 2000, :switch, @pink},
-    { 3000, :switch, @skyblue},
-    { 4000, :switch, @pink},
-    { 5000, :switch, @skyblue},
-    { 6000, :switch, @pink},
-    { 7000, :switch, @skyblue},
-    { 8000, :switch, @pink},
-    { 9000, :switch, @skyblue},
-    {10000, :fade,   @pink,  2000},
-    {12000, :fade,   @pink, 4000},
-    {16000, :fade,   @skyblue, 4000},
-    {20000, :fade,   @black, 1000},
-    {22000, :fade,   @black, 2000},
-    {23000, :rainbow}
-  ] |> Enum.map(expand_score)
-
-  @black_out [
-    {10, :switch,@black},
-    {1000, :switch,@black}
-  ] |> Enum.map(expand_score)
 
   defstruct maestro: nil
 
@@ -49,7 +25,12 @@ defmodule Flash.Manager do
     GenServer.cast __MODULE__, {:start_live, offset}
   end
 
-  def scores, do: @scores
+  def scores do
+    [
+      switch_cycle(20, [@pink, @skyblue, @blue], 350, 0),
+      fade_cycle(  30, [@pink, @skyblue, @blue], 474, 350*20)
+    ] |> List.flatten |> Enum.map(&expand_score/1)
+  end
 
   def current do
     GenServer.call __MODULE__, :current
@@ -77,33 +58,23 @@ defmodule Flash.Manager do
   end
 
   def handle_cast({:start_live, offset}, state = %{maestro: nil}) do
-    {:ok, pid} = Flash.Maestro.start_link(@scores, offset)
+    {:ok, pid} = Flash.Maestro.start_link(scores, offset)
 
     {:noreply, %{state | maestro: pid}}
   end
 
   def handle_cast({:start_live, offset}, state = %{maestro: pid}) do
     :fired = Flash.Maestro.fire(pid)
-    {:ok, new_pid} = Flash.Maestro.start_link(@scores, offset)
+    {:ok, new_pid} = Flash.Maestro.start_link(scores, offset)
 
     {:noreply, %{state | maestro: new_pid}}
-  end
-
-  def handle_cast(:switch_black, state = %{maestro: nil}) do
-    {:ok, pid} = Flash.Maestro.start_link(@black_out)
-
-    {:noreply, %{state | maestro: pid}}
   end
 
   def handle_cast(:switch_black, state = %{maestro: pid}) do
     :fired = Flash.Maestro.fire(pid)
-    {:ok, new_pid} = Flash.Maestro.start_link(@black_out)
+    {:ok, new_pid} = Flash.Maestro.start_link(black_out)
 
     {:noreply, %{state | maestro: new_pid}}
-  end
-
-  def handle_call(:current, _from, state = %{maestro: nil}) do
-    {:reply, %{}, state}
   end
 
   def handle_call(:current, _from, state = %{maestro: pid}) do
