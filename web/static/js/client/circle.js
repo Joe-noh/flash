3efed059bd250ee67b32e3bc9766d4a7ebad8169
Joe-noh <goflb.jh@gmail.com>
2015-12-23 03:16:15 +0900

    始点バラけさせて丸がにゅーんって

--- web/static/js/client/circle.js
+++ web/static/js/client/circle.js
@@ -8,19 +8,31 @@ class Circle {
     this.circleDom.css('width',  '0%');
     this.circleDom.css('height', '0%');
 
-    let height = $('body').height();
-    let width  = $('body').width();
+    let body = $('body');
+    let height = body.height();
+    let width  = body.width();
 
     let larger = ((height > width) ? height : width) * 2;
 
-    let initTop  = height / 4;
-    let initLeft = width / 3;
+    let initTop  = this._random(0.85*height, 0.15*height);
+    let initLeft = this._random(0.85*width,  0.15*width);
 
     this.circleDom.css('top',  initTop);
     this.circleDom.css('left', initLeft);
 
     this.circleDom.css('backgroundColor', color);
-    this.circleDom.velocity({width: larger, height: larger, top: initTop - larger/2, left: initLeft - larger/2}, duration);
+    this.circleDom.velocity({
+      width:  larger,
+      height: larger,
+      top:    initTop - larger/2,
+      left:   initLeft - larger/2
+    }, duration, () => {
+      this.backDom.css('backgroundColor', color);
+    });
+  }
+
+  _random(min, max) {
+    return Math.floor(Math.random() * (max - min) + min);
   }
 }
 
